cfb9490bc19e32e6117a5e6c9b7c34ef96d7a6ad
Pawel <pawel@liveprice.pl>
2025-07-13 16:58:00 +0200

    desktop header ready, and buttons of desktop too

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,47 @@
-import React from 'react';
-import './Header.scss';
+import React, { useState } from "react";
+import "./Header.scss";
+
+interface HeaderProps {
+  highlighted?: "hero" | "o-mnie" | "oferta" | "portfolio" | "kontakt" | "blog";
+  setHighlighted?: React.Dispatch<
+    React.SetStateAction<
+      "hero" | "o-mnie" | "oferta" | "portfolio" | "kontakt" | "blog"
+    >
+  >;
+}
 
 const Header: React.FC<HeaderProps> = () => {
-    return (
-        <header>
-            <div className={"header--wrapper"}>
-               <nav>
-                    <ul>
-                        <li>
-                            <a href="#o-mnie">O Mnie</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#oferta">Oferta</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#portfolio">Portfolio</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#kontakt">Kontakt</a>
-                        </li>
-                        
-                        <li>
-                            <a href="#blog">Blog</a>
-                        </li>
-                    </ul>
-                </nav>
+  const [highlighted, setHighlighted] = useState<HeaderProps["highlighted"]>("hero");
 
-                <a href="tel:884935095">
-                    884 935 095
-                </a>
-            </div>
-        </header>
-    )   
-}
+  return (
+    <header>
+      <div className={"header--wrapper"}>
+        <nav>
+          <ul>
+            {
+                ['o-mnie', 'oferta', 'portfolio', 'kontakt', 'blog'].map((item) => (
+                  <li key={item}>
+                    <a
+                      href={`#${item}`}
+                      onClick={() => setHighlighted(item as HeaderProps["highlighted"])}
+                      className={highlighted === item ? "active" : ""}
+                    >
+                      {
+                        item.toUpperCase().replace(/-/g, " ")
+                      }
+                    </a>
+                  </li>
+                ))
+            }
+          </ul>
+        </nav>
+
+        <a className={"btn btn-white"} href="tel:884935095">
+          884 935 095
+        </a>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
