922dcb55ec2c2206a0ecb67bd1a81fb0106efb67
Pawel <pawel@liveprice.pl>
2025-07-20 13:01:25 +0200

    framer-motion and working virtual scroll

---
+++ src/components/SectionPortfolio/Portfolios/PortfolioPathPro.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { motion, useScroll, useTransform, useMotionValue } from "framer-motion";
+
+const PortfolioPathPro = () => {
+  const sectionRef = React.useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({ target: sectionRef });
+  const height = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
+  return (
+    <div className="section__portfolio__content__project">
+      <div className="section__portfolio__content__project__name">PathPro</div>
+
+      <div className="section__portfolio__scroll" ref={sectionRef}>
+        <motion.div 
+            className="section__portfolio__scroll--dot"
+            style={{
+            top: height,
+            }}
+            >
+        </motion.div>
+
+        <motion.div
+          className="section__portfolio__scroll--highlight"
+          style={{
+            height,
+          }}
+        ></motion.div>
+      </div>
+
+      <div className="section__portfolio__content__project__description">
+        Kompleksowy projekt identyfikacji wizualnej dla marki specjalizującej
+        się w doradztwie zawodowym. Stworzyłam spójny system graficzny
+        obejmujący logo, materiały do social mediów, broszurę informacyjną oraz
+        slajdy prezentacyjne.
+        <StaticImage
+          src="../../../images/portfolio-pathpro-1.webp"
+          alt="pathpro main image"
+          loading="lazy"
+          placeholder="blurred"
+          layout="constrained"
+        />
+        <StaticImage
+          src="../../../images/portfolio-pathpro-2.webp"
+          alt="pathpro main image variable"
+          loading="lazy"
+          placeholder="blurred"
+          layout="constrained"
+        />
+        Motyw przewodni - gwiazda-kompas - symbolizuje kierunek, rozwój i
+        nowoczesne podejście do planowania kariery. Całość została
+        zaprojektowana z naciskiem na profesjonalizm, zaufanie i przejrzystość
+        wizualną. Projekt realizowany od podstaw - od koncepcji po finalne
+        makiety materiałów marketingowych.
+        <StaticImage
+          src="../../../images/portfolio-pathpro-3.webp"
+          alt="pathpro mobile mockup"
+          loading="lazy"
+          placeholder="blurred"
+          layout="constrained"
+        />
+        <StaticImage
+          src="../../../images/portfolio-pathpro-4.webp"
+          alt="pathpro banners"
+          loading="lazy"
+          placeholder="blurred"
+          layout="constrained"
+        />
+        <StaticImage
+          src="../../../images/portfolio-pathpro-5.webp"
+          alt="pathpro magazine mockup"
+          loading="lazy"
+          placeholder="blurred"
+          layout="constrained"
+        />
+        <StaticImage
+          src="../../../images/portfolio-pathpro-6.webp"
+          alt="pathpro card mockup"
+          loading="lazy"
+          placeholder="blurred"
+          layout="constrained"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioPathPro;
--- src/components/SectionPortfolio/SectionPortfolio.tsx
+++ src/components/SectionPortfolio/SectionPortfolio.tsx
@@ -1,47 +1,19 @@
 import * as React from "react";
 
 import { StaticImage } from "gatsby-plugin-image";
+import { motion, useScroll, useTransform } from "motion/react"
 
 import "./SectionPortfolio.scss";
-import "swiper/css";
-import "swiper/css/navigation";
+import PortfolioPathPro from "./Portfolios/PortfolioPathPro";
 
 const SectionPortfolio = () => {
-
   return (
     <div className="section__portfolio">
-      <div class="section__portfolio__scroll">
-        *
-      </div>
-
-      <div className="section__portfolio__content">
-          <div className="section__portfolio__content__project">
-            <div className="section__portfolio__content__project__name">
-              PathPro
-            </div>
-            <div className="section__portfolio__content__project__description">
-              Kompleksowy projekt identyfikacji wizualnej dla marki specjalizującej się w doradztwie zawodowym. Stworzyłam spójny system graficzny obejmujący logo, materiały do social mediów, broszurę informacyjną oraz slajdy prezentacyjne.
-              Motyw przewodni – gwiazda-kompas – symbolizuje kierunek, rozwój i nowoczesne podejście do planowania kariery. Całość została zaprojektowana z naciskiem na profesjonalizm, zaufanie i przejrzystość wizualną. Projekt realizowany od podstaw – od koncepcji po finalne makiety materiałów marketingowych.
-            
-
-              Kompleksowy projekt identyfikacji wizualnej dla marki specjalizującej się w doradztwie zawodowym. Stworzyłam spójny system graficzny obejmujący logo, materiały do social mediów, broszurę informacyjną oraz slajdy prezentacyjne.
-              Motyw przewodni – gwiazda-kompas – symbolizuje kierunek, rozwój i nowoczesne podejście do planowania kariery. Całość została zaprojektowana z naciskiem na profesjonalizm, zaufanie i przejrzystość wizualną. Projekt realizowany od podstaw – od koncepcji po finalne makiety materiałów marketingowych.
-            </div>
-          </div>
-
-          <div className="section__portfolio__content__project">
-            <div className="section__portfolio__content__project__name">
-              PathPro
-            </div>
-            <div className="section__portfolio__content__project__description">
-              Kompleksowy projekt identyfikacji wizualnej dla marki specjalizującej się w doradztwie zawodowym. Stworzyłam spójny system graficzny obejmujący logo, materiały do social mediów, broszurę informacyjną oraz slajdy prezentacyjne.
-              Motyw przewodni – gwiazda-kompas – symbolizuje kierunek, rozwój i nowoczesne podejście do planowania kariery. Całość została zaprojektowana z naciskiem na profesjonalizm, zaufanie i przejrzystość wizualną. Projekt realizowany od podstaw – od koncepcji po finalne makiety materiałów marketingowych.
-            
-
-              Kompleksowy projekt identyfikacji wizualnej dla marki specjalizującej się w doradztwie zawodowym. Stworzyłam spójny system graficzny obejmujący logo, materiały do social mediów, broszurę informacyjną oraz slajdy prezentacyjne.
-              Motyw przewodni – gwiazda-kompas – symbolizuje kierunek, rozwój i nowoczesne podejście do planowania kariery. Całość została zaprojektowana z naciskiem na profesjonalizm, zaufanie i przejrzystość wizualną. Projekt realizowany od podstaw – od koncepcji po finalne makiety materiałów marketingowych.
-            </div>
-          </div>
+      <div className="section__portfolio__content" >
+        <PortfolioPathPro />
+        <PortfolioPathPro />
+        <PortfolioPathPro />
+        <PortfolioPathPro />
       </div>
     </div>
   );
