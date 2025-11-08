7403ab5546a224130374ce1f297ebe371111a25b
Pawel <pawel@liveprice.pl>
2025-11-08 16:39:02 +0100

    hydration error fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useStaticQuery } from "gatsby";
 import "./Footer.scss";
 import InstagramIcon from '../../assets/icons/instagram.svg';
 import FacebookIcon from '../../assets/icons/facebook.svg';
@@ -7,12 +8,15 @@ import BehanceIcon from '../../assets/icons/behance.svg';
 
 
 const Footer: React.FC<FooterProps> = () => {
+  const [year, setYear] = React.useState("");
+  React.useEffect(() => setYear(String(new Date().getFullYear())), []);
+
   return (
     <footer>
       <div className="footer__wrapper">
         <h3>Stwórzmy wspólnie coś niesamowitego!</h3>
         <div className="footer__copyright">
-          <p>© Natalia Płocka {new Date().getFullYear()}</p>
+          <p>© Natalia Płocka {year}</p>
           <div className="footer__copyright--socials">
             <a href="https://www.instagram.com/nataluziu/" target="_blank" rel="noopener noreferrer">
               <InstagramIcon />
